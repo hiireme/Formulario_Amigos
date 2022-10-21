f0d4c35858337532d7e2c696261fee7b98db3e34
Regina Medina <reginaedna2127@gmail.com>
2022-10-21 11:50:13 -0500

    formulario

---
+++ formulario.js
@@ -0,0 +1,107 @@
+let amigos = [];
+
+let btnGuardar = document.querySelector("#btnGuardar");
+let btnCancelar = document.querySelector("#btnCancelar");
+
+let lista = document.querySelector(".listaAmigos");
+let formulario = document.querySelector("#formulario");
+
+pintar();
+
+function limpiar() {
+    formulario[0].value = "";
+    formulario[1].value = "";
+    formulario[2].value = "";
+    formulario[3].value = "";
+}
+
+function pintar() {
+    if (amigos.length > 0) {
+        lista.innerHTML = "";
+        amigos.forEach((contacto, index) => {
+            let amigo = document.createElement("div");
+            amigo.innerHTML = `<p>${contacto.nombre}</p><button class="muestraDetalles"><input type="hidden" value="${contacto.telefono}" />Detalles</button><button class="eliminarContacto" indice="${index}">Borrar</button>`;
+            lista.appendChild(amigo);
+        });
+        let botones = document.getElementsByClassName("muestraDetalles");
+        for (let i = 0; i < botones.length; i++) {
+            const element = botones[i];
+            element.addEventListener("click", () => {
+                showdetallesAmigos(element.children[0].value);
+            });
+        }
+        botones = document.getElementsByClassName("eliminarContacto");
+        for (let i = 0; i < botones.length; i++) {
+            const element = botones[i];
+            element.addEventListener("click", () => {
+                amigos.splice(element.getAttribute("indice"), 1);
+                pintar();
+            })
+        }
+    }
+    else {
+        lista.innerHTML = "<h2>No tenemos amigos</h2>";
+    }
+}
+
+function showdetallesAmigos(tel) {
+    let Detalles = document.getElementById("detallesAmigos");
+    let amigo = amigos.find(a => {
+        if (a.telefono == tel) {
+            return a;
+        }
+    });
+
+    Detalles.innerHTML = `<img src="${amigo.foto}"alt="">
+    <h3>${amigo.nombre}</h3>
+    <p><span>Telefono:</span>${amigo.telefono}</p>
+    <p><span>Correo:</span>${amigo.correo}</p>
+    <button id="btncerrar">Cerrar</button>`;
+    Detalles.classList.remove("oculto");
+
+    let btncerrar = document.querySelector("#btncerrar");
+    btncerrar.addEventListener("click", (event) => {
+        Detalles.classList.add("oculto")
+    });
+}
+
+btnCancelar.addEventListener("click", (event) => {
+    limpiar();
+    event.preventDefault();
+});
+
+btnGuardar.addEventListener("click", (event) => {
+
+    let contacto = {
+        nombre: formulario["nombre"].value,
+        telefono: formulario["telefono"].value,
+        correo: formulario["correo"].value,
+        foto: formulario["foto"].value,
+    };
+    amigos.push(contacto);
+    limpiar();
+    pintar();
+    event.preventDefault();
+
+    //VALIDACIÓN
+
+    let validacion = "";
+    function validacion(contacto) {
+        var campos_llenos = true;
+        if (document.getElementsByClassName("nombre").value == "") {
+            campos_llenos = false;
+        }
+        if (document.getElementsByClassName("telefono").value == "") {
+            campos_llenos = false;
+        }
+        if (document.getElementsByClassName("correro").value == "") {
+            campos_llenos = false;
+        }
+        if (document.getElementsByClassName("foto").value == "") {
+            campos_llenos = false;
+        }
+        
+        return validacion(campos_llenos = false)
+    };
+
+})
